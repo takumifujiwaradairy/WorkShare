1cc0880a79d55cad0bed171a1915554b34f7d90b
takumifujiwaradairy <89083036+takumifujiwaradairy@users.noreply.github.com>
2021-10-31 15:16:17 +0900

    Tweetモデルの単体テストを作成しよう

---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :user do
+    email {"e@gmail.com"}
+    name {"上田"}
+    password {"aA11111111"}
+  end
+end
---
+++ spec/models/tweet_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+RSpec.describe Tweet, type: :model do
+  let(:tweet) {FactoryBot.create(:tweet)}
+  feature "tweet" do
+    it "Userが存在し、:tatle,:tantou,が存在すれば有効であること" do
+      expect(tweet).to be_valid
+    end
+  end
+  feature "untweet" do
+    it "Userが存在しても、;tatleが存在しなければ無効であること" do
+      untweet = FactoryBot.build(:untweet)
+      untweet.valid?
+      expect(untweet.errors[:title]).to include("can't be blank")
+    end
+  end
+  feature "untweet2" do
+    it "Userが存在しても,;tantouが存在しなければ無効であること" do
+      untweet = FactoryBot.build(:untweet2)
+      untweet.valid?
+      expect(untweet.errors[:tantou]).to include("can't be blank")
+    end
+  end
+end
