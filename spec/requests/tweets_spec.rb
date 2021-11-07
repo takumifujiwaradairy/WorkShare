5d9a6b4ccd3f8fc4837b2dc3f99a4de56cace089
takumifujiwaradairy <89083036+takumifujiwaradairy@users.noreply.github.com>
2021-11-07 19:34:33 +0900

    TweetコントロラーのリクエストSpec

--- spec/requests/tweets_spec.rb
+++ spec/requests/tweets_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 RSpec.describe "Tweets", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /tweets" do
+    before do
+      @user = FactoryBot.create(:user)
+    end
+    it 'ログインしているuserでは200レスポンスを返すこと' do
+      sign_in @user
+      get tweets_path
+      expect(response).to have_http_status(200)
+    end
+    it 'ログインしてないuserでは302レスポンスを返すこと' do
+      get tweets_path
+      expect(response).to have_http_status(302)
+    end
   end
 end
