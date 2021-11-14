0ebfa4e4b4f0c49588f2fd513ec58269e5f41a50
takumifujiwaradairy <89083036+takumifujiwaradairy@users.noreply.github.com>
2021-11-15 07:33:15 +0900

    topページのデザインを作成しました。

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-
+  
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
-
+  config.assets.digest = false
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
