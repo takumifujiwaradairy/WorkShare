4463f7bef54200d290eae879b708b69415926045
takumifujiwaradairy <89083036+takumifujiwaradairy@users.noreply.github.com>
2021-11-22 23:36:46 +0900

    like機能を追加しました

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_211_122_034_931) do
+ActiveRecord::Schema.define(version: 20_211_122_133_454) do
+  create_table 'likes', force: :cascade do |t|
+    t.integer 'tweet_id', null: false
+    t.integer 'user_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['tweet_id'], name: 'index_likes_on_tweet_id'
+    t.index ['user_id'], name: 'index_likes_on_user_id'
+  end
+
   create_table 'tweets', force: :cascade do |t|
     t.string 'title'
     t.string 'tantou'
@@ -36,4 +45,7 @@ ActiveRecord::Schema.define(version: 20_211_122_034_931) do
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
+
+  add_foreign_key 'likes', 'tweets'
+  add_foreign_key 'likes', 'users'
 end
