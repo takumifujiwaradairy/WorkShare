e51d6cc0685e2bb11281ba75210dcc28f9939a71
takumifujiwaradairy <89083036+takumifujiwaradairy@users.noreply.github.com>
2021-12-17 21:25:29 +0900

    lineログインのリダイレクト修正

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,12 +1,11 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def index; end
   def line; basic_action end
 
   private
   def basic_action
     @omniauth = request.env["omniauth.auth"]
     if @omniauth.present?
-      puts User.column_names
-      puts @omniauth.inspect
       @profile = User.find_or_initialize_by(provider: @omniauth["provider"], uid: @omniauth["uid"])
       if @profile.email.blank?
         email = @omniauth["info"]["email"] ? @omniauth["info"]["email"] : "#{@omniauth["uid"]}-#{@omniauth["provider"]}@example.com"
@@ -16,7 +15,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in(:user, @profile)
     end
     flash[:notice] = "ログインしました"
-    redirect_to root_path
+    redirect_to tweet_path
   end
 
   def fake_email(uid, provider)
